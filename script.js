8d6660fffa95abf83ee7d8ffed4abb9fb0469394
anagtemboury <anagtemboury@uma.es>
2025-03-12 19:24:30 +0100

    Versión 1.0

---
+++ script.js
@@ -0,0 +1,7 @@
+// Obtener el botón por su ID
+const boton = document.getElementById('mi-boton');
+
+// Agregar un evento de clic
+boton.addEventListener('click', function() {
+    alert('¡Has hecho clic en el botón!');
+});
